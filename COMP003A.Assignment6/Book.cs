6878636fa2fb66d16d9e32d3bb841eb8cb3db3ee
Deegan Acevedo <deeganacevedo@my.whccd.edu>
2026-02-20 18:44:57 -0800

    Added Book class and constructor

---
+++ COMP003A.Assignment6/Book.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace COMP003A.Assignment6
+{
+    internal class Book
+    {
+        public string Title;
+        public int Pages;
+        public bool Completion;
+
+        public Book(string title, int pages, bool completion)
+        {
+            Title = title;
+            Pages = pages;
+            Completion = completion;
+        }
+
+
+    }
+}
