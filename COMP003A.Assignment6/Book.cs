f2e5fd70aaa93fb5c1bea4cf7b005f847367ca42
Deegan Acevedo <deeganacevedo@my.whccd.edu>
2026-02-20 20:11:42 -0800

    Added loop logic, basic options, and method in Book to display book info

--- COMP003A.Assignment6/Book.cs
+++ COMP003A.Assignment6/Book.cs
@@ -1,22 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using static System.Net.WebRequestMethods;
 
 namespace COMP003A.Assignment6
 {
     internal class Book
     {
+        public int BookNumber;
         public string Title;
         public int Pages;
         public bool Completion;
 
-        public Book(string title, int pages, bool completion)
+        public Book(int bookNumber, string title, int pages, bool completion)
         {
+            BookNumber = bookNumber;
             Title = title;
             Pages = pages;
             Completion = completion;
         }
 
-
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Item {BookNumber}: Title={Title}, Pages={Pages}, Completed?={Completion}");
+        }
     }
 }
--- COMP003A.Assignment6/Program.cs
+++ COMP003A.Assignment6/Program.cs
@@ -4,9 +4,39 @@
     {
         static void Main(string[] args)
         {
+            Book book1 = new Book(1, "Boooook", 125, false);
+            Book book2 = new Book(2, "Kooooob", 521, true);
+            Book book3 = new Book(3, "Okbkbko", 215, true);
+
             while (true)
             {
+                Console.Write("1. Display All\n2. Update One Item\n 3. Exit\nEnter choice: ");
 
+                string initialChoice = Console.ReadLine();
+
+                switch (initialChoice)
+                {
+                    // Display
+                    case "1":
+                        book1.DisplayInfo();
+                        book2.DisplayInfo();
+                        book3.DisplayInfo();
+                        continue;
+
+                    // Update
+                    case "2":
+                        continue;
+
+                    // Exit
+                    case "3":
+                        Console.WriteLine("Goodbye!");
+                        break;
+
+                    default:
+                        Console.WriteLine("Unknown choice, try again.");
+                        continue;
+                }
+                break;
             }
         }
     }
